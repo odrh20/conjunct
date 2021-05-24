c53687d9d3ffd46a0e91560c80fedfa5d3ed543c
Oli <odrh20@bath.ac.uk>
2021-05-25 00:51:00 +0800

    Added deterministic transition function

--- PDA.py
+++ PDA.py
@@ -50,6 +50,7 @@ class PDA:
         return f"States: {self.states} \nInput Alphabet: {self.input_alphabet} \nStack Alphabet: {self.stack_alphabet} \
                \nTransitions: {self.transitions} \nInitial State: {self.initial_state} \nInitial Stack Symbol: {self.initial_stack_symbol}"
 
+
 class PDAConfiguration:
     def __init__(self, pda, input_string, current_state=None, current_stack=None, remaining_input=None, computation=None, config_dict=None):
         if current_state is None:
@@ -70,12 +71,40 @@ class PDAConfiguration:
         self.remaining_input = remaining_input
         self.computation = computation
         self.config_dict = config_dict
-        #self.update_config_dict()
+        self.update_config_dict()
 
 
     def get_config(self):
         # A PDA configuration is a triple of (current state, remaining input, stack contents)
         return self.current_state, self.remaining_input, self.current_stack
+
+    def get_config_tuple(self):
+        return self.current_state, self.remaining_input, tuple(self.current_stack)
+
+    def update_config_dict(self):
+
+        # Updates dictionary which maps configurations of (current state, remaining input, current stack) to a list of
+        # available transitions (letter to read, string to push to stack, next state).
+
+        # Only need to update if the configuration is not already in the dictionary and it has an available transition
+        if self.get_config_tuple() not in self.config_dict and self.current_state in self.pda.transitions and \
+                self.current_stack[0] in self.pda.transitions[self.current_state]:
+
+            # Check for transitions reading the next letter
+            if self.remaining_input[0] in self.pda.transitions[self.current_state][self.current_stack[0]]:
+                self.config_dict[self.get_config_tuple()] = []
+                for next_state, push_stack in self.pda.transitions[self.current_state][self.current_stack[0]][self.remaining_input[0]]:
+                    self.config_dict[self.get_config_tuple()].append((self.remaining_input[0], push_stack, next_state))
+
+            # Check for transitions reading 'e'
+            if self.remaining_input[0] != 'e' and 'e' in self.pda.transitions[self.current_state][self.current_stack[0]]:
+                if self.get_config_tuple() not in self.config_dict:
+                    self.config_dict[self.get_config_tuple()] = []
+                for next_state, push_stack in self.pda.transitions[self.current_state][self.current_stack[0]]['e']:
+                    self.config_dict[self.get_config_tuple()].append(('e', push_stack, next_state))
+
+
+
 
     def is_accepting_config(self):
         # An accepting configuration is when the stack is empty and remaining input is empty
@@ -87,16 +116,19 @@ class PDAConfiguration:
         # Empty cases: if the pop_symbol is 'e', don't remove anything. If the push_string is 'e', don't append
         # anything.
 
-        if len(self.current_stack) == 0 or (pop_symbol != 'e' and self.current_stack[0] != pop_symbol):
-            print("Error. Stack transition not appropriate.")
+        if len(self.current_stack) == 0:
+            print("Error. Stack already empty")
+            return
+
+        if self.current_stack[0] != pop_symbol:
+            print("Error. Symbol to pop is not on the stack.")
             return
 
         # Pop:
-        if pop_symbol != 'e':
-            if len(self.current_stack) == 1:
-                self.current_stack = ['e']
-            else:
-                self.current_stack = self.current_stack[1:]
+        if len(self.current_stack) == 1:
+            self.current_stack = ['e']
+        else:
+            self.current_stack = self.current_stack[1:]
 
         # Push:
         if push_string == 'e':
@@ -123,9 +155,32 @@ class PDAConfiguration:
         # Update stack
         self.stack_transition(pop_symbol, push_string)
 
-        #self.update_config_dict()
+        self.update_config_dict()
+
+    def run_deterministic_transitions(self):
+        """
+        From a given PDA configuration, runs transitions as long as there is only one available.
+        Updates the configuration, and returns Accept/Reject booleans.
+        """
 
 
+        # Check if in an accepting configuration
+        if self.is_accepting_config():
+            return True, False
+
+        # Check if in a rejecting configuration
+        if self.get_config_tuple() not in self.config_dict:
+            return False, True
+
+        # Check if the next transition is non-deterministic
+        if len(self.config_dict[self.get_config_tuple()]) > 1:
+            return False, False
+
+        # Else, run transition, update the configuration and computation, then recurse.
+        letter, push_string, state = self.config_dict[self.get_config_tuple()][0]
+        self.run_transition(letter, state, self.current_stack[0], push_string)
+        self.computation.append(self.get_config())
+        return self.run_deterministic_transitions()
 
 
 
@@ -135,28 +190,26 @@ pda = PDA(
     stack_alphabet={'Z', 'a'},
     transitions={
         'q0':                         # Start state
-            {'a':                     # Letter to read
-                 {'Z':                # Stack symbol to pop
+            {'Z':                     # Stack symbol to pop
+                 {'a':                # Letter to read
                       {('q1', 'aZ')}  # Set of (next state, stack string to push) transitions. If only one, deterministic transition.
                   }
              },
         'q1':
             {'a':
                  {'a':
-                      {('q1', 'aa')}
+                      {('q1', 'aa')},
+                  'b' : {('q2', 'e')}
                   },
-             'b':
-                 {'a':
-                      {('q2', 'e')}
-                  }
+
              },
         'q2':
-            {'b':
-                 {'a':
+            {'a':
+                 {'b':
                       {('q2', 'e')}
                   },
-             'e':
-                 {'Z':
+             'Z':
+                 {'e':
                       {('q3', 'e')}
                   }
              }
@@ -165,11 +218,8 @@ pda = PDA(
     initial_stack_symbol='Z'
 )
 
-print(pda)
-pda_config = PDAConfiguration(pda, 'ab')
+pda_config = PDAConfiguration(pda, 'aaabbb')
 
-print(pda_config.current_stack)
 
-pda_config.stack_transition('Z', 'e')
-print(pda_config.current_stack)
-#pda_configuration.run_transition('a', 'q1', )
+print(pda_config.run_deterministic_transitions())
+print(pda_config.computation)
